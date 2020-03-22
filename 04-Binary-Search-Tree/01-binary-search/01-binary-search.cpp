bb29ee4eaacf1894dab737b433bada82e592d3bc
LinW <43196127+lnwn@users.noreply.github.com>
2020-03-22 19:09:18 +0800

    Add bs and bst insert method

---
+++ 04-Binary-Search-Tree/01-binary-search/01-binary-search.cpp
@@ -0,0 +1,30 @@
+﻿// 01-binary-search.cpp : 二分查找。
+// 2020/03/07
+// 二分查找法，数组必须是有序的
+
+#include <iostream>
+
+using namespace std;
+// 在有序数组 arr 中，查找 target
+// 如果找到 target，返回相应的索引 index
+// 如果没有找到 target，返回 -1
+template<typename T>
+int binarySearch(T arr[], int n, T target) {
+	int l = 0, r = n - 1;// 在 [l...r] 中查找 target
+	while (l <= r) {
+		//x int mid = (l + r) / 2;
+		int mid = l + (r - l) / 2;// 避免 (l+r) 溢出
+		if (arr[mid] == target)
+			return mid;
+		if (target < arr[mid])
+			r = mid - 1;
+		else
+			l = mid + 1;
+	}
+	return -1;
+}
+
+int main()
+{
+	return 0;
+}
---
+++ 04-Binary-Search-Tree/02-insert/02-insert.cpp
@@ -0,0 +1,68 @@
+﻿// 02-insert.cpp : binary search tree - insert。
+// 2020/03/22
+
+// 插入的元素与待插入位置本身元素重合时，用新元素覆盖原元素
+#include <iostream>
+
+using namespace std;
+
+// binary search tree
+template<typename Key, typename Value>
+class BST {
+
+private:
+	struct Node {
+		Key key;
+		Value value;
+		Node *left;
+		Node *right;
+
+		Node(Key key, Value value) {
+			this->key = key;
+			this->value = value;
+			this->left = this->right = NULL;
+		}
+	};
+
+	Node *root;// 根节点
+	int count;// 树中节点个数
+
+public:
+	// 构造函数，默认构造一棵空树
+	BST() {
+		root = NULL;
+		count = 0;
+	}
+	// 析构函数
+	~BST() {
+		// todo ~BST()
+	}
+	// 返回节点个数
+	int size() {
+		return count;
+	}
+	// 向树中插入一个新（key，value）
+	void insert(Ke.y key, Value value) {
+		root = insert(root, key, value);
+	}
+
+private:
+	// 使用递归算法
+	// 返回插入新节点后二叉搜索树的根
+	Node* insert(Node *node, Key key, Value value) {
+		if (node == null) {
+			count++;
+			return new Node(key, value);
+		}
+		if (key == node->key)
+			node->value = value;
+		else if (key < node->key)
+			node->left = insert(node->left, key, value);
+		else
+			node->right = insert(node->right, key, value);
+		return node;
+	}
+};
+int main() {
+	return 0;
+}
